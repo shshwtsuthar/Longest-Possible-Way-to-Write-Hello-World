71dc851981b411ec1a83925305a6d62c61c86aba
shshwtsuthar <shshwtsuthar@gmail.com>
2021-01-07 10:54:26 +0530

    initital commit

---
+++ hello.py
@@ -0,0 +1,51 @@
+#Converting h to Binary because Samuel has a Small PP
+h=1
+if h==1.:
+    h="H"
+    
+else:
+    print ("Samuel doesnt have a PP at all")
+
+e=2
+if e==2:
+    e="E"
+    
+else:
+    print ("Samuel has a PP, its just Small... as small as a Pea.")
+
+l=3
+if l==3:
+    l="L"
+    
+else:
+    print ("Samuel is a Noob Coder.")
+
+o=4
+if o==4:
+    o="O"
+    
+else:
+    print ("Samuel Learnt Coding from WhiteHatJR. This Secret is now unveiled, sorry Samuel, but you had a Small PP so this .")
+
+w=5
+if w==5:
+    w="W"
+    
+else:
+    print ("i have nothing to say.")
+    
+r=6
+if r==6:
+    r="R"
+    
+else:
+    print ("shut the fuck up")
+    
+d=7
+if d==7:
+    d="D"
+    
+else:
+    print ("Program Finished :)")
+    
+print (h, e, l, l, o, w, o, r, l, d)
